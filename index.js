98c9cbd810829a6ca51fe1bccd0ed008c17f9579
luciayin9944 <luxiayin94@gmail.com>
2025-04-25 11:27:45 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code here!
+
+function displayPosts(posts) {
+    const postList = document.querySelector('#post-list')
+    posts.forEach(post => {
+        const postItem = document.createElement('li');
+        const postTitle = document.createElement('h1')
+        const postContent = document.createElement('p');
+
+        postTitle.textContent = post.title;
+        postContent.textContent = post.body;
+        //Wrong: postItem.appendChild(postTitle, postContent);
+        postItem.appendChild(postTitle);
+        postItem.appendChild(postContent);
+        postList.appendChild(postItem);
+    });
+}
+
+async function fetchAndDisplayPosts() {
+    try{
+        const response = await
+        fetch('https://jsonplaceholder.typicode.com/posts');
+
+        const posts = await response.json();
+        //displayPosts(posts.slice(0, 10));
+        displayPosts(posts);
+    } catch(error) {
+        console.error('Error fetching user data:', error);
+    }
+}
+
+fetchAndDisplayPosts();
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then(response => response.json())
+// .then(posts => console.log(posts))
+// .catch(error => {
+//     console.error('Error fetching user data:', error);
+// });
